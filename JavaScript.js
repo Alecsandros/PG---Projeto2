c2e933d306e1f6086a1652e39cb9ffc9665cb36d
Alecsandro Silva da Conceicao Junior <ascj@windows.cin.ufpe.br>
2017-12-18 11:59:38 -0300

    Mudança nas variaveis

--- JavaScript.js
+++ JavaScript.js
@@ -2,10 +2,10 @@ var canvas = document.getElementById("canvas");
 //var ctx = canvas.getContext("2d");
 var DesenharPontos = [];
 
-var auxArray = [];
-var camera = [];
-var objeto = [];
-var iluminacao = [];
+var Auxarray = [];
+var Camera = [];
+var Objeto = [];
+var Iluminacao = [];
 
 //impressão
 var aux1 = '';
@@ -37,14 +37,14 @@ var Il;
 var n;
 
 var U;
-var NormalTriangulo = [];
-var NormalVertice= [];
+var Normaltriangulo = [];
+var Normalvertice= [];
 
 var I;
-var PontosVista = [];
-var PlVista;
+var Pontosvista = [];
+var Plvista;
 
-var PontosTela = [];
+var Pontostela = [];
 var larguraJanela = 1000; //MUDAR N ESQUECER
 var alturaJanela = 1000; //MUDAR N ESQUECER
 
@@ -74,17 +74,17 @@ window.onload = function () {
         var txtReader = new FileReader();
 
         cfgReader.onload = function (e) {
-            camera = cfgReader.result.split('\n');
-            C = setVetor(camera[0]);
-            N = setVetor(camera[1]);
-            V = setVetor(camera[2]);
-            var vet = setVetor(camera[3]);
+            Camera = cfgReader.result.split('\n');
+            C = setVetor(Camera[0]);
+            N = setVetor(Camera[1]);
+            V = setVetor(Camera[2]);
+            var vet = setVetor(Camera[3]);
             d = vet.a;
             hx = vet.b;
             hy = vet.c;
 
             //imprimir
-            aux2 = '1º Carregar arquivos (objeto(s), iluminação e câmera): \n Camera: \n';
+            aux2 = '1º Carregar arquivos (Objeto(s), iluminação e câmera): \n Camera: \n';
             aux2 += 'C: ' + C.a + ' ' + C.b + ' ' + C.c + '\n';
             aux2 += 'Vetor N: ' + N.a + ' ' + N.b + ' ' + N.c + '\n';
             aux2 += 'Vetor V: ' + V.a + ' ' + V.b + ' ' + V.c + '\n';
@@ -105,66 +105,66 @@ window.onload = function () {
             U = produtoVetorial(V,N);
 
             //Monta a matriz de mudança de base com U, V e N
-            MontarMatriz();
+            montarMatriz();
 
-            /*imprimir();*/
+            imprimir();
 
         }
         cfgReader.readAsText(cfgTobeRead);
 
         byuReader.onload = function (e) {
-            objeto = byuReader.result.split('\n');  //de 1 a vet[0]-1
-            var vet = objeto[0].split(' ');
+            Objeto = byuReader.result.split('\n');  //de 1 a vet[0]-1
+            var vet = Objeto[0].split(' ');
             for(var i = 0; i < vet[0]; i ++){
-                Pontos[i] = setVetor(objeto[i+1]);
+                Pontos[i] = setVetor(Objeto[i+1]);
             }
             vet0 = parseInt(vet[0]) + 1;
             for(var i = 0; i < vet[1]; i++){ // de vet[0] a vet[1]-1
-                Triangulos[i] = setVetor(objeto[i + vet0]);
+                Triangulos[i] = setVetor(Objeto[i + vet0]);
             }
 
             //A normal dos vertices inicializada com 0
             for(var i = 0; i < Pontos.length; i++){
-                NormalVertice[i] = {a: 0, b:0, c:0};
+                Normalvertice[i] = {a: 0, b:0, c:0};
             }
 
 
             // A normal do triangulo
             for (var i = 0; i < Triangulos.length; i++){
-                NormalTriangulo[i] = normalTriangulo(Triangulos[i]);
+                Normaltriangulo[i] = normalTriangulo(Triangulos[i]);
                 normalVertice(i);
             }
 
             //Cada ponto para coordenada de vista 
-            CoodenadasVistaPontos();
+            coordenadasVistaPontos();
 
             //Cada ponto para coordenada de tela
-            CoordenadasTela()
+            coordenadasTela()
 
             //Chamando o scanline para imprimir os pontos dos triangulos na tela
-            Scanline();
+            scanLine();
 
-           /* imprimir();*/
+            imprimir();
             
         }
         byuReader.readAsText(byuTobeRead);
 
         txtReader.onload = function (e) {
-            iluminacao = txtReader.result.split('\n');
-            Pl = setVetor(iluminacao[0]);
-            ka = iluminacao[1];
-            Ia = setVetor(iluminacao[2]);
-            kd = iluminacao[3];
-            Od = setVetor(iluminacao[4]);
-            ks = iluminacao[5];
-            Il = setVetor(iluminacao[6]);
-            n = iluminacao[7];
+            Iluminacao = txtReader.result.split('\n');
+            Pl = setVetor(Iluminacao[0]);
+            ka = Iluminacao[1];
+            Ia = setVetor(Iluminacao[2]);
+            kd = Iluminacao[3];
+            Od = setVetor(Iluminacao[4]);
+            ks = Iluminacao[5];
+            Il = setVetor(Iluminacao[6]);
+            n = Iluminacao[7];
 
             //Posicao da fonte de luz de coordenadas de mundo para coordenadas de vista
-            CoodenadaVistaPl();
+            coordenadaVistaPl();
 
 
-            /*imprimir();*/
+            imprimir();
 
         }
         txtReader.readAsText(txtTobeRead);
@@ -175,8 +175,8 @@ window.onload = function () {
 }
 
 function setVetor(vetor){
-    var auxArray = vetor.split(' ');
-    return {a: parseFloat(auxArray[0]), b: parseFloat(auxArray[1]), c: parseFloat(auxArray[2])};
+    var Auxarray = vetor.split(' ');
+    return {a: parseFloat(Auxarray[0]), b: parseFloat(Auxarray[1]), c: parseFloat(Auxarray[2])};
 }
 
 function produtoInterno(vetor1,vetor2) {
@@ -209,7 +209,7 @@ function normalTriangulo(triangulo) {
     var vertice3 = Pontos[triangulo.c - 1];
     var P3P1 = {a: vertice3.a - vertice1.a, b: vertice3.b - vertice1.b, c: vertice3.c - vertice1.c };
     var P2P1 = {a: vertice2.a - vertice1.a, b: vertice2.b - vertice1.b, c: vertice2.c - vertice1.c };
-    var vetorial = produtoVetorial(P3P1, P2P1);
+    var vetorial = produtoVetorial(P2P1, P3P1);
     return normalizacao(vetorial);
 }
 
@@ -217,12 +217,12 @@ function normalVertice(i){
     var p1 = Triangulos[i].a - 1;
     var p2 = Triangulos[i].b - 1;
     var p3 = Triangulos[i].c - 1;
-    var Ta = NormalTriangulo[i].a;
-    var Tb = NormalTriangulo[i].b;
-    var Tc = NormalTriangulo[i].c;
-    NormalVertice[p1] = {a: NormalVertice[p1].a + Ta, b: NormalVertice[p1].b + Tb,c: NormalVertice[p1].c + Tc};
-    NormalVertice[p2] = {a: NormalVertice[p2].a + Ta, b: NormalVertice[p2].b + Tb,c: NormalVertice[p2].c + Tc};
-    NormalVertice[p3] = {a: NormalVertice[p3].a + Ta, b: NormalVertice[p3].b + Tb,c: NormalVertice[p3].c + Tc};
+    var Ta = Normaltriangulo[i].a;
+    var Tb = Normaltriangulo[i].b;
+    var Tc = Normaltriangulo[i].c;
+    Normalvertice[p1] = {a: Normalvertice[p1].a + Ta, b: Normalvertice[p1].b + Tb,c: Normalvertice[p1].c + Tc};
+    Normalvertice[p2] = {a: Normalvertice[p2].a + Ta, b: Normalvertice[p2].b + Tb,c: Normalvertice[p2].c + Tc};
+    Normalvertice[p3] = {a: Normalvertice[p3].a + Ta, b: Normalvertice[p3].b + Tb,c: Normalvertice[p3].c + Tc};
 }
 
 // Não conseguimos usar 11 para representar Linha 1, coluna 1, então as colunas serão representadas de "aa" a "cc"
@@ -230,46 +230,46 @@ function normalVertice(i){
 // [ ba bb bc ]
 // [ ca cb cc ]
 
-function MontarMatriz(){
+function montarMatriz(){
     I = {aa: U.a, ab: U.b, ac: U.c, ba: V.a, bb: V.b, bc: V.c, ca: N.a, cb: N.b, cc: N.c};
 }
 
-function CoodenadasVistaPontos(){
+function coordenadasVistaPontos(){
     for(var i = 0; i < Pontos.length; i++){
         var matriz = {aa: Pontos[i].a - C.a, ba: Pontos[i].b - C.b, ca: Pontos[i].c - C.c};
-        PontosVista[i] = multmatrizes(matriz);;
+        Pontosvista[i] = multMatrizes(matriz);;
     }
 }
 
-function CoodenadaVistaPl(){
+function coordenadaVistaPl(){
     var matriz = {aa: Pl.a - C.a, ba: Pl.b - C.b, ca: Pl.c - C.c};
-    PlVista = multmatrizes(matriz);
+    Plvista = multMatrizes(matriz);
 }
 
-function multmatrizes(matriz){
+function multMatrizes(matriz){
     var x = (I.aa * matriz.aa) + (I.ab * matriz.ba) + (I.ac * matriz.ca);
     var y = (I.ba * matriz.aa) + (I.bb * matriz.ba) + (I.bc * matriz.ca);
     var z = (I.ca * matriz.aa) + (I.cb * matriz.ba) + (I.cc * matriz.ca);
     return {a: x, b: y, c: z};
 }
 
-function CoordenadasTela(){
-    for(var i = 0; i < PontosVista.length; i++){
-        var a = (d/hx) * PontosVista[i].a;
-        var b = (d/hy) * PontosVista[i].b;
-        a = a / PontosVista[i].c;
-        b = b / PontosVista[i].c;
+function coordenadasTela(){
+    for(var i = 0; i < Pontosvista.length; i++){
+        var a = (d/hx) * Pontosvista[i].a;
+        var b = (d/hy) * Pontosvista[i].b;
+        a = a / Pontosvista[i].c;
+        b = b / Pontosvista[i].c;
         a = (a + 1) * larguraJanela / 2;
         b = (1 - b) * alturaJanela / 2;
-        PontosTela[i] = {x: parseInt(a), y: parseInt(b)};
+        Pontostela[i] = {x: parseInt(a), y: parseInt(b)};
     }
 }
 
-function Scanline(){
+function scanLine(){
     for(var i = 0; i < Triangulos.length; i++){
-    	var p1 = PontosTela[parseInt(Triangulos[i].a) -1];
-        var p2 = PontosTela[parseInt(Triangulos[i].b) -1];
-        var p3 = PontosTela[parseInt(Triangulos[i].c) -1];
+    	var p1 = Pontostela[parseInt(Triangulos[i].a) -1];
+        var p2 = Pontostela[parseInt(Triangulos[i].b) -1];
+        var p3 = Pontostela[parseInt(Triangulos[i].c) -1];
         var v1, v2, v3, v4;
         var ord = ordem(p1, p2, p3);
         v1 = ord[2];
@@ -327,9 +327,6 @@ function Bottom(v1, v2, v3){
     var invislope2 = (v3.x - v1.x) / (v3.y - v1.y);
     var curx1 = v1.x;
     var curx2 = v1.x; 
-    t += invislope1 + ' ' + invislope2 + ' ' + curx1 + ' ' + curx2;
-    var content = document.getElementById('content'); 
-    content.innerText = t; 
     for(var scanlineY = v1.y; scanlineY <= v2.y; scanlineY++){
         desenharPontos(curx1, scanlineY, curx2);
         curx1 += invislope1;
@@ -376,28 +373,28 @@ function imprimir (){
 
 
     for (var i = 0; i < Triangulos.length; i++){
-        aux4 += 'NormalTriangulo ' + (i+1) + ': ' + NormalTriangulo[i].a + '  ' + NormalTriangulo[i].b + '  ' + NormalTriangulo[i].c + '\n';
+        aux4 += 'Normaltriangulo ' + (i+1) + ': ' + Normaltriangulo[i].a + '  ' + Normaltriangulo[i].b + '  ' + Normaltriangulo[i].c + '\n';
     }
     for(var i = 0; i < Pontos.length; i++){
-        aux4 += 'NormalVertice ' + (i+1) + ': ' + NormalVertice[i].a + '  ' + NormalVertice[i].b + '  ' + NormalVertice[i].c + '\n';
+        aux4 += 'Normalvertice ' + (i+1) + ': ' + Normalvertice[i].a + '  ' + Normalvertice[i].b + '  ' + Normalvertice[i].c + '\n';
     }
     auxT += '\n\n 4º Calcular a normal do triângulo e dos vérties: \n ' + aux4; 
 
-    aux5 = 'Cordenada de vista Pl: ' + PlVista.a + '  ' + PlVista.b + '  ' + PlVista.c + '\n';
+    aux5 = 'Cordenada de vista Pl: ' + Plvista.a + '  ' + Plvista.b + '  ' + Plvista.c + '\n';
 
     for(var i = 0; i < Pontos.length; i++){
-        aux5 += 'PontosVista ' + (i+1) + ': ' + PontosVista[i].a + '  ' + PontosVista[i].b + '  ' + PontosVista[i].c + '\n';
+        aux5 += 'Pontosvista ' + (i+1) + ': ' + Pontosvista[i].a + '  ' + Pontosvista[i].b + '  ' + Pontosvista[i].c + '\n';
     }
     auxT += '\n\n 5º Fazer a mudança de coordenadas de mundo para coordenadas de vista: \n ' + aux5; 
 
-    var aux6 = '2º ENTREGA \n' + PontosTela.length;
-    for (var i  = 0; i < PontosTela.length; i++){
-        aux6 += 'PontosTela ' + (i+1) + ': ' + PontosTela[i].x + ' ' + PontosTela[i].y + '\n'; 
+    var aux6 = '2º ENTREGA \n';
+    for (var i  = 0; i < Pontostela.length; i++){
+        aux6 += 'Pontostela ' + (i+1) + ': ' + Pontostela[i].x + ' ' + Pontostela[i].y + '\n'; 
     }
 
-    for(var i = 0; i < PontosDesenhar.length; i++){
+    /*for(var i = 0; i < PontosDesenhar.length; i++){
     	aux7 += 'PontosDesenhar ' + (i+1) + ': ' + PontosDesenhar.x + ' ' + PontosDesenhar.y + '\n';
-    }
+    }*/
 
     auxT += aux6;
     auxT += aux7;
